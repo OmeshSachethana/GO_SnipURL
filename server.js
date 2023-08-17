8e45f5efc477863228102494a123e3a91ee2b36f
OmeshSachethana <sachethana12@gmail.com>
2023-08-17 16:47:44 +0530

    created urlShortener endpoint

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 5000;
 
 app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
 
 app.get('/', (req, res) => {
     res.render('index');
